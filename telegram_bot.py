644c77110bf0e6d184ac8812cd9e6a6e763a5644
MaxSmile44 <afmaxim89@gmail.com>
2025-01-10 13:54:08 +0300

    Publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,11 +3,16 @@ import os
 import telegram
 
 
+def tg_bot_send(tg_token, chat_id, path):
+    bot = telegram.Bot(token=tg_token)
+    bot.send_photo(chat_id=chat_id, photo=open(path, 'rb'))
+
 def main():
     load_dotenv()
     tg_token = os.getenv('TG_TOKEN')
-    bot = telegram.Bot(token=tg_token)
-    bot.send_message(text='Hello World!', chat_id=-1002498810711)
+    chat_id = -1002498810711
+    path = './images/spacex_7.jpg'
+    tg_bot_send(tg_token, chat_id, path)
 
 
 if __name__ == '__main__':
